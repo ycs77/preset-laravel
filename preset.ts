61396ed7382aec7c56070fcbd6a7334d90248105
Lucas Yang <yangchenshin77@gmail.com>
2021-06-16 03:26:02 +0800

    Update preset

--- preset.ts
+++ preset.ts
@@ -2,9 +2,7 @@ import { color, Preset } from 'apply'
 
 Preset.setName('Laravel Initialize')
 
-Preset.group((preset) => {
-  preset.extract('default')
-}).withTitle('Extracting templates...')
+Preset.extract('default').withTitle('Extracting templates...')
 
 Preset.edit('.editorconfig')
   .withTitle(`Updating ${color.magenta('.editorconfig')}...`)
